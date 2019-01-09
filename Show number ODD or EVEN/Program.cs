43a07f2516dfc7dded044c5f6a0ed766f9afaf12
Behfar Baghovvat <46481959+BehfarBaghovvat@users.noreply.github.com>
2019-01-10 03:19:46 +0330

    Add project files.

---
+++ Show number ODD or EVEN/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Show_number_ODD_or_EVEN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Number;
+
+            System.Console.WriteLine("          ======================SHOW NUMBER OOD OR EVEN======================");
+
+            System.Console.WriteLine();
+
+            System.Console.Write
+                ("Please enter number: ");
+
+            Number = System.Convert.ToInt32
+                (System.Console.ReadLine());
+
+            System.Console.WriteLine();
+
+            int Result = Number % 2;
+
+            if (Result==0)
+            {
+                System.Console.Write
+                    ("The number {0} is EVEN",Number);
+
+                System.Console.WriteLine();
+                System.Console.WriteLine();
+            }
+
+            else
+            {
+                System.Console.Write
+                    ("The number {0} is ODD", Number);
+
+                System.Console.WriteLine();
+                System.Console.WriteLine();
+            }  
+            
+            
+            
+
+            System.Console.Write("Press [ENTER] To Exit...");
+            System.Console.ReadLine();
+
+
+        }
+    }
+}
